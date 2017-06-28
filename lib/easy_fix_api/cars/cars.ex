8b444da879e757dbfd3ecedde9d6163ca7b69631
Geovane Fedrecheski <geonnave@gmail.com>
2017-06-27 21:54:27 -0300

    Remove unused functions from Cars context module

--- lib/easy_fix_api/cars/cars.ex
+++ lib/easy_fix_api/cars/cars.ex
@@ -8,96 +8,12 @@ defmodule EasyFixApi.Cars do
 
   alias EasyFixApi.Cars.Brand
 
-  @doc """
-  Returns the list of brands.
-
-  ## Examples
-
-      iex> list_brands()
-      [%Brand{}, ...]
-
-  """
   def list_brands do
     Repo.all(Brand)
   end
 
-  @doc """
-  Gets a single brand.
-
-  Raises `Ecto.NoResultsError` if the Brand does not exist.
-
-  ## Examples
-
-      iex> get_brand!(123)
-      %Brand{}
-
-      iex> get_brand!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_brand!(id), do: Repo.get!(Brand, id)
 
-  @doc """
-  Creates a brand.
-
-  ## Examples
-
-      iex> create_brand(%{field: value})
-      {:ok, %Brand{}}
-
-      iex> create_brand(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_brand(attrs \\ %{}) do
-    %Brand{}
-    |> brand_changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a brand.
-
-  ## Examples
-
-      iex> update_brand(brand, %{field: new_value})
-      {:ok, %Brand{}}
-
-      iex> update_brand(brand, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_brand(%Brand{} = brand, attrs) do
-    brand
-    |> brand_changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Brand.
-
-  ## Examples
-
-      iex> delete_brand(brand)
-      {:ok, %Brand{}}
-
-      iex> delete_brand(brand)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_brand(%Brand{} = brand) do
-    Repo.delete(brand)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking brand changes.
-
-  ## Examples
-
-      iex> change_brand(brand)
-      %Ecto.Changeset{source: %Brand{}}
-
-  """
   def change_brand(%Brand{} = brand) do
     brand_changeset(brand, %{})
   end
@@ -110,96 +26,13 @@ defmodule EasyFixApi.Cars do
 
   alias EasyFixApi.Cars.Model
 
-  @doc """
-  Returns the list of models.
-
-  ## Examples
-
-      iex> list_models()
-      [%Model{}, ...]
-
-  """
   def list_models do
     Repo.all(Model)
+    |> Repo.preload(:brand)
   end
 
-  @doc """
-  Gets a single model.
+  def get_model!(id), do: Repo.get!(Model, id) |> Repo.preload(:brand)
 
-  Raises `Ecto.NoResultsError` if the Model does not exist.
-
-  ## Examples
-
-      iex> get_model!(123)
-      %Model{}
-
-      iex> get_model!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_model!(id), do: Repo.get!(Model, id)
-
-  @doc """
-  Creates a model.
-
-  ## Examples
-
-      iex> create_model(%{field: value})
-      {:ok, %Model{}}
-
-      iex> create_model(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_model(attrs \\ %{}) do
-    %Model{}
-    |> model_changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a model.
-
-  ## Examples
-
-      iex> update_model(model, %{field: new_value})
-      {:ok, %Model{}}
-
-      iex> update_model(model, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_model(%Model{} = model, attrs) do
-    model
-    |> model_changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Model.
-
-  ## Examples
-
-      iex> delete_model(model)
-      {:ok, %Model{}}
-
-      iex> delete_model(model)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_model(%Model{} = model) do
-    Repo.delete(model)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking model changes.
-
-  ## Examples
-
-      iex> change_model(model)
-      %Ecto.Changeset{source: %Model{}}
-
-  """
   def change_model(%Model{} = model) do
     model_changeset(model, %{})
   end
