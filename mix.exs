7ae0f697db75e283bc678ef46d09fa9fcd2065c6
Geovane Fedrecheski <geonnave@gmail.com>
2017-08-12 16:52:26 -0300

    Bump versions: elixir to 1.5 and phoenix to 3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EasyFixApi.Mixfile do
   def project do
     [app: :easy_fix_api,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      start_permanent: Mix.env == :prod,
@@ -22,11 +22,11 @@ defmodule EasyFixApi.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc", override: true},
+      {:phoenix, "~> 1.3", override: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
+      {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:nimble_csv, "~> 0.1.0"},
