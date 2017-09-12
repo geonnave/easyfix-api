8e42961b29092eb1bcfc1237abc23885254f2ac6
Geovane Fedrecheski <geonnave@gmail.com>
2017-09-11 21:28:01 -0300

    Change setup_docker script

--- setup_docker.sh
+++ setup_docker.sh
@@ -3,9 +3,7 @@
 docker-compose build web_dev
 docker-compose up -d web_dev
 docker-compose run web_dev mix do deps.get, compile
-docker-compose run web_dev mix ecto.create
-docker-compose run web_dev mix ecto.migrate
-docker-compose run web_dev mix run priv/repo/seeds.exs
+docker-compose run web_dev mix ecto.reseed
 docker-compose run web_dev mix run priv/repo/static_data_seeds.exs
 docker-compose restart web_dev
 docker-compose run web_dev
