db8219cb828f9d1c3821f91a678fe772fd691ee5
Geovane Fedrecheski <geonnave@gmail.com>
2017-10-21 02:05:47 -0200

    Rename static_data_seeds to prod_seeds on setup_docker script

--- setup_docker.sh
+++ setup_docker.sh
@@ -6,6 +6,6 @@ docker-compose build web_dev
 
 docker-compose run web_dev mix do deps.get, compile
 docker-compose run web_dev mix ecto.reset
-docker-compose run web_dev mix run priv/repo/static_data_seeds.exs
+docker-compose run web_dev mix run priv/repo/prod_seeds.exs
 
 docker-compose up -d web_dev
