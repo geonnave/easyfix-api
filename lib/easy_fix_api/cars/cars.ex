8e28a6eab39ddfaf12bb338814d4b0c64d287c6e
Geovane Fedrecheski <geonnave@gmail.com>
2017-10-22 18:08:46 -0200

    Fix vehicle plate upcase

--- lib/easy_fix_api/cars/cars.ex
+++ lib/easy_fix_api/cars/cars.ex
@@ -66,13 +66,17 @@ defmodule EasyFixApi.Cars do
          vehicle_assoc_attrs <- Helpers.apply_changes_ensure_atom_keys(vehicle_assoc_changeset) do
 
       model = get_model!(vehicle_assoc_attrs[:model_id])
+
       vehicle_id_number = if vehicle_changeset.changes[:vehicle_id_number] do
         String.upcase(vehicle_changeset.changes[:vehicle_id_number])
+      end
+      plate = if vehicle_changeset.changes[:plate] do
+        String.upcase(vehicle_changeset.changes[:plate])
       end
 
       {:ok, vehicle} =
         vehicle_changeset
-        |> put_change(:plate, String.upcase(vehicle_changeset.changes[:plate]))
+        |> put_change(:plate, plate)
         |> put_change(:vehicle_id_number, vehicle_id_number)
         |> put_assoc(:model, model)
         |> Repo.insert()
