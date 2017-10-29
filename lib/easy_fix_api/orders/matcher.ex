c834c587ee5c4e2bbe4a88103a6f422f93c07f5e
Geovane Fedrecheski <geonnave@gmail.com>
2017-10-29 02:25:17 -0200

    Fix: customer data should not appear to garage if there is no quote

--- lib/easy_fix_api/orders/matcher.ex
+++ lib/easy_fix_api/orders/matcher.ex
@@ -10,8 +10,9 @@ defmodule EasyFixApi.Orders.Matcher do
 
   def list_orders_matching_garage(garage) do
     Orders.list_orders
-    |> Enum.filter(fn %{diagnosis: diagnosis} ->
-      diagnosis_matches_garage_categories?(diagnosis, garage.garage_categories)
+    |> Enum.filter(fn order = %{diagnosis: diagnosis} ->
+      diagnosis_matches_garage_categories?(diagnosis, garage.garage_categories) &&
+      order.inserted_at >= garage.inserted_at
     end)
   end
 
