c0269526dcd22435ab560ea17a3dce479b9e6cd6
Geovane Fedrecheski <geonnave@gmail.com>
2017-10-29 19:56:03 -0200

    Change timeout values

--- config/prod.exs
+++ config/prod.exs
@@ -42,8 +42,8 @@ config :easy_fix_api, EasyFixApi.Mailer,
 config :easy_fix_api, :order_states,
   started: [],
   created_with_diagnosis: [
-    timeout: [value: [minutes: 3], event: :to_quoted_by_garages],
-    fixer_timeout: [value: [minutes: 1], event: :to_quoted_by_garages],
+    timeout: [value: [minutes: 7], event: :to_quoted_by_garages],
+    fixer_timeout: [value: [minutes: 5], event: :to_quoted_by_garages],
   ],
   not_quoted_by_garages: [final_state: true, macro_state: :canceled],
   quoted_by_garages: [
